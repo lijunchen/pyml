8305ed4503157a25f51a1da00b8071859500e628
Li Junchen <lijunchen95@outlook.com>
2025-04-11 23:31:14 +0800

    x

--- crates/wasm-app/src/lib.rs
+++ crates/wasm-app/src/lib.rs
@@ -8,7 +8,7 @@ pub fn execute(src: &str) -> String {
     let core = compiler::compile_match::compile(&env, &tast);
     let mut buffer = String::new();
     let _result = compiler::interpreter::eval(&im::HashMap::new(), &mut buffer, &core);
-    return buffer;
+    buffer
 }
 
 #[wasm_bindgen]
@@ -17,8 +17,6 @@ pub fn compile_to_core(src: &str) -> String {
     let ast = parser.parse(src).unwrap();
     let (tast, env) = compiler::typer::check_file(ast);
     let core = compiler::compile_match::compile(&env, &tast);
-    let mut buffer = String::new();
-    let _result = compiler::interpreter::eval(&im::HashMap::new(), &mut buffer, &core);
-    let core_str = core.to_pretty(&env, 120);
-    return core_str;
+
+    core.to_pretty(&env, 120)
 }

6c6f8a412a6ccb8b2171523ad656a6c93b683c2e
Li Junchen <lijunchen95@outlook.com>
2025-04-14 22:05:23 +0800

    remove scaffolding tests

--- crates/compiler/src/tests/mod.rs
+++ crates/compiler/src/tests/mod.rs
@@ -1,11 +1,6 @@
 use cst::cst::CstNode;
 use parser::{debug_tree, syntax::MySyntaxNode};
 
-mod ast_test;
-mod interp_test;
-mod smoke_test;
-mod typer_test;
-
 #[test]
 fn test_cases() -> anyhow::Result<()> {
     let root_dir = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
